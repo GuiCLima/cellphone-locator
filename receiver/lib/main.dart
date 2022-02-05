4f7819837a142309395cecd0043ba218f31cbe21
GuiCLima <gui7play@gmail.com>
2022-02-05 20:07:24 -0300

    minor changes

--- receiver/lib/main.dart
+++ receiver/lib/main.dart
@@ -1,112 +1,111 @@
-import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:geolocator_app/env.dart';
-import 'package:sms/sms.dart';
-import 'dart:convert';
-import 'package:screen/screen.dart';
+  import 'package:flutter/material.dart';
+  import 'package:geolocator/geolocator.dart';
+  import 'package:geolocator_app/env.dart';
+  import 'package:sms/sms.dart';
+  import 'dart:convert';
+  import 'package:screen/screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Locator',
-      home: MyHomePage(title: 'Locator'),
-      debugShowCheckedModeBanner: false,
-    );
+  void main() {
+    runApp(const MyApp());
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key key, this.title}) : super(key: key);
+  class MyApp extends StatelessWidget {
+    const MyApp({Key key}) : super(key: key);
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  SmsReceiver receiver = SmsReceiver();
-  final jsonEncoder = const JsonEncoder();
-
-
-
-
-  bool isActive;
-
-
-  Future<Position> _determinePosition() async {
-    bool serviceEnable;
-    LocationPermission permission;
-
-    serviceEnable = await Geolocator.isLocationServiceEnabled();
-    if(!serviceEnable) {
-      return Future.error('Location services are disabled');
+    @override
+    Widget build(BuildContext context) {
+      return const MaterialApp(
+        title: 'Flutter Locator',
+        home: MyHomePage(title: 'Locator'),
+        debugShowCheckedModeBanner: false,
+      );
     }
-    permission = await Geolocator.checkPermission();
-    if(permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
+  }
+
+  class MyHomePage extends StatefulWidget {
+    const MyHomePage({Key key, this.title}) : super(key: key);
+
+    final String title;
+
+    @override
+    State<MyHomePage> createState() => _MyHomePageState();
+  }
+
+  class _MyHomePageState extends State<MyHomePage> {
+
+    SmsReceiver receiver = SmsReceiver();
+    final jsonEncoder = const JsonEncoder();
+
+    bool isActive;
+    final int smsInterval = 40;
+
+    Future<Position> _determinePosition() async {
+      bool serviceEnable;
+      LocationPermission permission;
+
+      serviceEnable = await Geolocator.isLocationServiceEnabled();
+      if(!serviceEnable) {
+        return Future.error('Location services are disabled');
+      }
+      permission = await Geolocator.checkPermission();
       if(permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
-    }
-    if(permission == LocationPermission.deniedForever) {
-      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
-    }
-    return await Geolocator.getCurrentPosition();
-
-  }
-
-  Future<String> _getPosition() async {
-    Position myPosition = await _determinePosition();
-
-    Map<String, double> locationData = {
-      'altitude': myPosition.altitude,
-      'longitude' : myPosition.longitude,
-      'latitude' : myPosition.latitude,
-    };
-    return jsonEncoder.convert(locationData);
-  }
-
-  void _sendMessage(String message, String address) {
-    SmsSender sender = SmsSender();
-    sender.sendSms(SmsMessage(address, message));
-  }
-
-  Future<void> _runSmsRoutine() async {
-    String position = await _getPosition();
-    receiver.onSmsReceived.listen((SmsMessage msg) async{
-      if(msg.address == '+5511$senderNumber' &&  msg.body == activationCode) {
-        isActive = true;
-        while(isActive == true){
-          await Future.delayed(const Duration(seconds: 40), () {
-            _sendMessage(position, senderNumber);
-          });
+        permission = await Geolocator.requestPermission();
+        if(permission == LocationPermission.denied) {
+          return Future.error('Location permissions are denied');
         }
-      } else if(msg.address == '+5511$senderNumber' &&  msg.body == deactivationCode) {
-        isActive = false;
       }
-    });
-  }
+      if(permission == LocationPermission.deniedForever) {
+        return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+      }
+      return await Geolocator.getCurrentPosition();
 
-  @override
-  void initState() {
-    Screen.keepOn(true);
-    _runSmsRoutine(); 
-    super.initState();
-  }
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.black,
-    );
+    Future<String> _getPosition() async {
+      Position myPosition = await _determinePosition();
+
+      Map<String, double> locationData = {
+        'altitude': myPosition.altitude,
+        'longitude' : myPosition.longitude,
+        'latitude' : myPosition.latitude,
+      };
+      return jsonEncoder.convert(locationData);
+    }
+
+    void _sendMessage(String message, String address) {
+      SmsSender sender = SmsSender();
+      sender.sendSms(SmsMessage(address, message));
+    }
+
+    Future<void> _runSmsRoutine() async {
+      String position = await _getPosition();
+      receiver.onSmsReceived.listen((SmsMessage msg) async {
+        if(msg.address == '+5511$senderNumber' &&  msg.body == activationCode) {
+           _sendMessage(position, senderNumber);
+          isActive = true;
+          while (isActive == true) {
+            await Future.delayed(Duration(seconds: smsInterval), () {
+              _sendMessage(position, senderNumber);
+            });
+          }
+        } else if (msg.address == '+5511$senderNumber' &&  msg.body == deactivationCode) {
+          isActive = false;
+        }
+      });
+    }
+
+    @override
+    void initState() {
+      /* this keeps the device screen always on */
+      Screen.keepOn(true);
+      _runSmsRoutine(); 
+      super.initState();
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return const Scaffold(
+        backgroundColor: Colors.black,
+      );
+    }
   }
-}
